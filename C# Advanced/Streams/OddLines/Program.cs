27988172368743d6c1b72c92c85c2178ceceb402
Obelq <georgi.engineer@gmail.com>
2017-06-06 18:02:13 +0300

    Streams

---
+++ C# Advanced/Streams/OddLines/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    class Program
+    {
+
+        private const string TestFile = "test.txt";
+
+        public static void Main()
+        {
+            PrepareTestFile();
+            PrintOddLines();
+        }
+
+        private static void PrintOddLines()
+        {
+            using (StreamReader reader = new StreamReader(TestFile))
+            {
+                string line = reader.ReadLine();
+                int lineCounter = 0;
+                while (line != null)
+                {
+
+                    if (lineCounter % 2 == 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineCounter++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        static void PrepareTestFile()
+        {
+            using (StreamWriter writer = new StreamWriter(TestFile))
+            {
+                for (int i = 0; i < 99; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        writer.WriteLine($"{i} - even");
+                    }
+                    else
+                    {
+                        writer.WriteLine($"{i} - odd");
+                    }
+                }
+            }
+        }
+    }
+}
+
