06b9eda6d28bb7186a2275547bc847de1d57a130
Georgi Ivaylov Georgiev <georgi4c@gmail.com>
2017-08-19 19:49:53 +0300

    Exam Preps added

--- C# OOP Advanced/Hell-Skeleton/Hell-Skeleton/Hell/IO/ConsoleReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class ConsoleReader
-{
-    public string ReadLine()
-    {
-        return Console.ReadLine();
-    }
-}
--- C# OOP Advanced/Hell-Skeleton/Hell-Skeleton/Hell/Interfaces/IHero.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IHero
-{
-    string Name { get; }
-}
